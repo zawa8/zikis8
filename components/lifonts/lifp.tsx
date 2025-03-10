3c034a5888bb2f6d6bb29d3bbc897452d025ac9d
viml kumar <vimalkg@gmail.com>
2025-03-10 10:42:32 +0530

    mck82.vercel.app zikis8.vercel.app fb.com/ztrabc

--- components/lifonts/lifp.tsx
+++ components/lifonts/lifp.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 import data from './data.json';
 import { v_to_f } from './vtof';
