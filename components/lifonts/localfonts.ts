c0e056e41ae89e7d3221733dacda2ac679478279
viml kumar <vimalkg@gmail.com>
2025-03-22 15:50:02 +0530

    mck82.vercel.app zikis8.vercel.app fb.com/ztrabc

--- components/lifonts/localfonts.ts
+++ components/lifonts/localfonts.ts
@@ -3,12 +3,12 @@ import localFont from "next/font/local";
 export const binaryv15 = localFont({ src: "./fonts/15_woff2/binaryv15.woff2", display: "swap", });
 export const ing15m = localFont({ src: "./fonts/15m_woff2/ing15m.woff2", display: "swap", });
 export const ing15 = localFont({ src: "./fonts/15_woff2/ing15.woff2", display: "swap", });
-export const iing15 = localFont({ src: "./fonts/15_woff2/iing15.woff2", display: "swap", });
-export const ing25 = localFont({ src: "./fonts/25_woff2/ing25.woff2", display: "swap", });
-export const ing4 = localFont({ src: "./fonts/4_woff2/ing4.woff2", display: "swap", });
+// export const iing15 = localFont({ src: "./fonts/15_woff2/iing15.woff2", display: "swap", });
+// export const ing25 = localFont({ src: "./fonts/25_woff2/ing25.woff2", display: "swap", });
+// export const ing4 = localFont({ src: "./fonts/4_woff2/ing4.woff2", display: "swap", });
 /////////////////
 export const hindi15 = localFont({ src: "./fonts/15_woff2/hindi15.woff2", display: "swap", });
-export const ihindi15 = localFont({ src: "./fonts/15_woff2/ihindi15.woff2", display: "swap", });
+// export const ihindi15 = localFont({ src: "./fonts/15_woff2/ihindi15.woff2", display: "swap", });
 export const bangla15 = localFont({ src: "./fonts/15_woff2/bangla15.woff2", display: "swap", });
 export const gurmukhi15 = localFont({ src: "./fonts/15_woff2/gurmukhi15.woff2", display: "swap", });
 export const guzrati15 = localFont({ src: "./fonts/15_woff2/guzrati15.woff2", display: "swap", });
@@ -20,17 +20,17 @@ export const kannada15 = localFont({ src: "./fonts/15_woff2/kannada15.woff2", di
 export const malayalam15 = localFont({ src: "./fonts/15_woff2/malayalam15.woff2", display: "swap", });
 export const sinhala15 = localFont({ src: "./fonts/15_woff2/sinhala15.woff2", display: "swap", });
 /////////////////
-export const hindi25 = localFont({ src: "./fonts/25_woff2/hindi25.woff2", display: "swap", });
-export const bangla25 = localFont({ src: "./fonts/25_woff2/bangla25.woff2", display: "swap", });
-export const gurmukhi25 = localFont({ src: "./fonts/25_woff2/gurmukhi25.woff2", display: "swap", });
-export const oriya25 = localFont({ src: "./fonts/25_woff2/oriya25.woff2", display: "swap", });
-export const korean25 = localFont({ src: "./fonts/25_woff2/korean25.woff2", display: "swap", });
-export const guzrati25 = localFont({ src: "./fonts/25_woff2/guzrati25.woff2", display: "swap", });
-export const tamil25 = localFont({ src: "./fonts/25_woff2/tamil25.woff2", display: "swap", });
-export const telugu25 = localFont({ src: "./fonts/25_woff2/telugu25.woff2", display: "swap", });
-export const kannada25 = localFont({ src: "./fonts/25_woff2/kannada25.woff2", display: "swap", });
-export const malayalam25 = localFont({ src: "./fonts/25_woff2/malayalam25.woff2", display: "swap", });
-export const sinhala25 = localFont({ src: "./fonts/25_woff2/sinhala25.woff2", display: "swap", });
+// export const hindi25 = localFont({ src: "./fonts/25_woff2/hindi25.woff2", display: "swap", });
+// export const bangla25 = localFont({ src: "./fonts/25_woff2/bangla25.woff2", display: "swap", });
+// export const gurmukhi25 = localFont({ src: "./fonts/25_woff2/gurmukhi25.woff2", display: "swap", });
+// export const oriya25 = localFont({ src: "./fonts/25_woff2/oriya25.woff2", display: "swap", });
+// export const korean25 = localFont({ src: "./fonts/25_woff2/korean25.woff2", display: "swap", });
+// export const guzrati25 = localFont({ src: "./fonts/25_woff2/guzrati25.woff2", display: "swap", });
+// export const tamil25 = localFont({ src: "./fonts/25_woff2/tamil25.woff2", display: "swap", });
+// export const telugu25 = localFont({ src: "./fonts/25_woff2/telugu25.woff2", display: "swap", });
+// export const kannada25 = localFont({ src: "./fonts/25_woff2/kannada25.woff2", display: "swap", });
+// export const malayalam25 = localFont({ src: "./fonts/25_woff2/malayalam25.woff2", display: "swap", });
+// export const sinhala25 = localFont({ src: "./fonts/25_woff2/sinhala25.woff2", display: "swap", });
 ///////////////////
 /*//
 bangla15    guzrati15  kannada15  malayalam15  sinhala15  telugu15
--- components/lifonts/vtof.ts
+++ components/lifonts/vtof.ts
@@ -1,42 +1,42 @@
 import {
-	ing15,ing15m, iing15, ing25, binaryv15, ing4, ihindi15,
+	ing15,ing15m, binaryv15, // ing4, ihindi15, iing15, ing25,
 	bangla15, guzrati15, kannada15, malayalam15, sinhala15, telugu15, 
 	gurmukhi15, hindi15, korean15, oriya15, tamil15,
-	bangla25, guzrati25, kannada25, malayalam25, sinhala25, telugu25, 
-	gurmukhi25, hindi25, korean25, oriya25, tamil25,
+	//bangla25, guzrati25, kannada25, malayalam25, sinhala25, telugu25, 
+	// gurmukhi25, hindi25, korean25, oriya25, tamil25,
 } from "./localfonts";
 
 export const v_to_f = (sval: string): string => {
     switch (sval) {
       case "ing15": return ing15.className;
       case "ing15m": return ing15m.className;
-      case "iing15": return iing15.className;
-      case "ihindi15": return ihindi15.className;
-      case "ing4": return ing4.className;
-      case "ing25": return ing25.className;
+      //case "iing15": return iing15.className;
+      //case "ihindi15": return ihindi15.className;
+      //case "ing4": return ing4.className;
+      //case "ing25": return ing25.className;
       case "binaryv15": return binaryv15.className;
       case "hindi15": return hindi15.className;
-      case "hindi25": return hindi25.className;
+      //case "hindi25": return hindi25.className;
       case "bangla15": return bangla15.className;
-      case "bangla25": return bangla25.className;
+      //case "bangla25": return bangla25.className;
       case "korean15": return korean15.className;
-      case "korean25": return korean25.className;
+      //case "korean25": return korean25.className;
       case "sinhala15": return sinhala15.className;
-      case "sinhala25": return sinhala25.className;
+      //case "sinhala25": return sinhala25.className;
       case "tamil15": return tamil15.className;
-      case "tamil25": return tamil25.className;
+      //case "tamil25": return tamil25.className;
       case "kannada15": return kannada15.className;
-      case "kannada25": return kannada25.className;
+      //case "kannada25": return kannada25.className;
       case "malayalam15": return malayalam15.className;
-      case "malayalam25": return malayalam25.className;
+      //case "malayalam25": return malayalam25.className;
       case "telugu15": return telugu15.className;
-      case "telugu25": return telugu25.className;
+      //case "telugu25": return telugu25.className;
       case "oriya15": return oriya15.className;
-      case "oriya25": return oriya25.className;
+      //case "oriya25": return oriya25.className;
       case "gurmukhi15": return gurmukhi15.className;
-      case "gurmukhi25": return gurmukhi25.className;
+      //case "gurmukhi25": return gurmukhi25.className;
       case "guzrati15": return guzrati15.className;
-      case "guzrati25": return guzrati25.className;
+      //case "guzrati25": return guzrati25.className;
       default: return hindi15.className;
     }
 };
