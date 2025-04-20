0c286c304a869dc63e3d10ebca90d77bc8aa410a
viml kumar <vimalkg@gmail.com>
2025-04-20 07:55:48 +0530

    ztr8.vercel.app mck82.vercel.app fb.com/ztrabc x.com/etphor

--- components/lifonts/lifp.tsx
+++ components/lifonts/lifp.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
 import data from './data.json';
-import { w2f } from './w2f';
+import { loklfont_2_fontcn } from './loklfont_2_fontcn';
 
 interface ScriptOption { olabel: string; ovalue: string;
   sfonts: Array<{ olabel: string; ovalue: string }>;
@@ -27,7 +27,7 @@ const FontPicker: React.FC = () => {
     if (sfontObject) { setBodyFont(sfontObject.ovalue); }
   };
   const setBodyFont = (sval: string) => { document.body.className = "";
-    const sf = w2f(sval); document.body.classList.add(sf); document.body.classList.add("antialiased");
+    const sf = loklfont_2_fontcn(sval); document.body.classList.add(sf); document.body.classList.add("antialiased");
   };
   return (
     <div style={{ width: 400, marginBottom: 20, color: 'black', backgroundColor: 'white' }} >
@@ -35,7 +35,7 @@ const FontPicker: React.FC = () => {
         onChange={handleScriptChange}
 		getOptionLabel={(x) => x.olabel} getOptionValue={(x) => x.ovalue}
       />
-	  <Select id='fontsel' placeholder="languagefont.select" value={sfont} options={sfontlist}
+	  <Select id='fontsel' className='hidden' isDisabled placeholder="languagefont.select" value={sfont} options={sfontlist}
         onChange={handleSFontChange}
 		getOptionLabel={(x) => x.olabel} getOptionValue={(x) => x.ovalue}
       />
--- components/lifonts/localfonts.ts
+++ components/lifonts/localfonts.ts
@@ -1,17 +1,17 @@
 import localFont from "next/font/local";
-/////////////////
-export const binaryw115b = localFont({ src: "./fonts/bigwoff/115b/binaryw115b.woff2", display: "swap", });
-export const ing115b = localFont({ src: "./fonts/bigwoff/115b/ing115b.woff2", display: "swap", });
-/////////////////
-export const hindi115b = localFont({ src: "./fonts/bigwoff/115b/hindi115b.woff2", display: "swap", });
-export const bangla115b = localFont({ src: "./fonts/bigwoff/115b/bangla115b.woff2", display: "swap", });
-export const gurmukhi115b = localFont({ src: "./fonts/bigwoff/115b/gurmukhi115b.woff2", display: "swap", });
-export const guzrati115b = localFont({ src: "./fonts/bigwoff/115b/guzrati115b.woff2", display: "swap", });
-export const oriya115b = localFont({ src: "./fonts/bigwoff/115b/oriya115b.woff2", display: "swap", });
-export const korean115b = localFont({ src: "./fonts/bigwoff/115b/korean115b.woff2", display: "swap", });
-export const tamil115b = localFont({ src: "./fonts/bigwoff/115b/tamil115b.woff2", display: "swap", });
-export const telugu115b = localFont({ src: "./fonts/bigwoff/115b/telugu115b.woff2", display: "swap", });
-export const kannada115b = localFont({ src: "./fonts/bigwoff/115b/kannada115b.woff2", display: "swap", });
-export const malayalam115b = localFont({ src: "./fonts/bigwoff/115b/malayalam115b.woff2", display: "swap", });
-export const sinhala115b = localFont({ src: "./fonts/bigwoff/115b/sinhala115b.woff2", display: "swap", });
+///////////////// nonindik bilo
+export const binarywenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/binarywenglosoftw8asc.woff2", display: "swap", });
+export const inglishenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/inglishenglosoftw8asc.woff2", display: "swap", });
+export const koreanenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/koreanenglosoftw8asc.woff2", display: "swap", });
+///////////////// indik bilo
+export const hindienglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/hindienglosoftw8asc.woff2", display: "swap", });
+export const banglaenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/banglaenglosoftw8asc.woff2", display: "swap", });
+export const gurmukhienglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/gurmukhienglosoftw8asc.woff2", display: "swap", });
+export const guzratienglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/guzratienglosoftw8asc.woff2", display: "swap", });
+export const oriyaenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/oriyaenglosoftw8asc.woff2", display: "swap", });
+export const tamilenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/tamilenglosoftw8asc.woff2", display: "swap", });
+export const teluguenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/teluguenglosoftw8asc.woff2", display: "swap", });
+export const kannadaenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/kannadaenglosoftw8asc.woff2", display: "swap", });
+export const malayalamenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/malayalamenglosoftw8asc.woff2", display: "swap", });
+export const sinhalaenglosoftw8asc = localFont({ src: "./fonts/hscii/englosoftw8/englosoftw8asc/sinhalaenglosoftw8asc.woff2", display: "swap", });
 
